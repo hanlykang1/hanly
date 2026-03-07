ad25c250e72580082907805271ab0dae3cdcb979
hanlykang1 <hanly_kang@qq.com>
2026-03-07 14:53:11 +0800

    更新

--- app/src/main/java/com/hanly/pw/MainActivity.kt
+++ app/src/main/java/com/hanly/pw/MainActivity.kt
@@ -7,17 +7,14 @@ import android.content.Intent
 import android.content.SharedPreferences
 import android.net.Uri
 import android.os.Bundle
-import android.view.Gravity
 import android.view.ViewGroup
 import android.view.WindowManager
-import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.SeekBar
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.hanly.pw.R
 import com.hanly.pw.ui.MainActivityLayout
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
